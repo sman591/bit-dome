6eae90379b1e1819abd59b8b2c3cf2d1a92c7372
Stuart Olivera <stuart@stuartolivera.com>
2015-03-28 16:59:43 -0400

    Added routing & navigation

--- bit-dome.js
+++ bit-dome.js
@@ -1,4 +1,30 @@
 if (Meteor.isClient) {
+
+  Router.configure({
+    layoutTemplate: 'ApplicationLayout'
+  });
+
+  Router.route('/', function () {
+    this.render('Home');
+  }, {
+    name: 'home'
+  });
+
+  Router.route('/about', function () {
+    this.render('About');
+  }, {
+    name: 'about'
+  });
+
+  Template.navItems.helpers({
+    // http://robertdickert.com/blog/2014/05/09/set-up-navigation-with-iron-router-and-bootstrap/
+    activeIfTemplateIs: function (template) {
+      var currentRoute = Router.current();
+      return currentRoute &&
+        template === currentRoute.lookupTemplate() ? 'active' : '';
+    }
+  });
+
   // counter starts at 0
   Session.setDefault('counter', 0);
 
